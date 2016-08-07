1e12c725c54dc7338dd346cc8734ddf1233a61bf
kowsalya11 <kowsalya996@gmail.com>
2016-08-07 11:32:52 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,20 @@
+public class Program {
+    public static void main(String[] args) {
+
+	// Create int array.
+	int[] array = new int[5];
+
+	// Assign first three elements.
+	array[0] = 1;
+	array[1] = 10;
+	array[2] = 100;
+
+	// Loop over elements.
+	for (int i = 0; i < array.length; i++) {
+	    // Get value.
+	    int value = array[i];
+	    // Print value.
+	    System.out.println(value);
+	}
+    }
+}
